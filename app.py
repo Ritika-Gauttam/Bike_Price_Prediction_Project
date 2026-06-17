b44038de2af47f6ef8bd1ba2fccb308d8dc3b4a8
Ritika-Gauttam <ritikagauttam19@gmail.com>
2026-06-17 15:50:41 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # firstly create old_bike virtual environment python -m venv old_bike
 # then run requirements.txt by pip install -r requirements.txt 
 from flask import Flask , url_for , request , render_template
+import joblib
+model = joblib.load(r"models\model_rfr.lb")
 app = Flask(__name__)
 @app.route('/')    #@means decorator means we are redefining  , / means home root 
 def home():
@@ -9,7 +11,7 @@ def home():
 # def intro():
 #     return "hello my name is ritika"
 
-@app.route('/index')  # / k aage kuch likhne ka mtlb root k aage is path pr to agr hme yh run krna h to url m aage /krke likhna pdega
+@app.route('/index')  # / k aage kuch likhne ka mtlb root k aage is path pr to agr hme yh run krna h to url m aage /krke likhna pdega***/=
 def index():
     return render_template('index.html')
 
@@ -29,6 +31,35 @@ def project():
     return render_template('project.html')
 
 
+@app.route("/project", methods = {'GET',"POST"})
+def predict():
+    if request.method == "POST":      # post is used to get data from frontend
+        brand_name = request.form['brand_name']
+        owner = int(request.form['owner'])
+        age = int(request.form['age'])
+        power = int(request.form['power'])
+        kms_driven = int(request.form['kms_driven'])
+
+       
+
+        brand_dict= {
+                    'TVS':1,  'Royal Enfield':2,  'Triumph':3, 'Yamaha':4,
+                    'Honda':5, 'Hero':6,   'Bajaj':7 ,     'Suzuki':8,
+            'Benelli':9,             'KTM':10,        'Mahindra':11,        'Kawasaki':12,
+            'Ducati':13,         'Hyosung':14, 'Harley-Davidson':15,            'Jawa':16,
+                'BMW':17,          'Indian':18,         'Rajdoot':19,             'LML':20,
+            'Yezdi':21,              'MV':22,           'Ideal': 23 
+    }
+
+    brand_name = brand_dict.get(brand_name)
+    pred = model.predict([[brand_name,owner,age , power ,kms_driven]])
+    # print("Outcome :- ",[brand_name,owner,age , power ,kms_driven])
+    print("Prediction:>>>>>>>>>>>",pred)
+    return render_template("project.html",prediction = pred)
+
+
+
+        
 if __name__ == "__main__":
     app.run(debug = True)
 
